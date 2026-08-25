92f30d9a7f6970efed347e01910e3641c483e4b4
ragnokv2-droid <ragnokv2@gmail.com>
2026-08-25 02:28:05 -0300

    Create MetaPixel.tsx

---
+++ src/components/MetaPixel.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Script from "next/script";
+
+const PIXEL_ID = "1049853073957255"; // ← troque pelo seu ID
+
+export default function MetaPixel() {
+  return (
+    <>
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '${1049853073957255}');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src={`https://www.facebook.com/tr?id=${1049853073957255}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+    </>
+  );
+}
+
+// Helper para disparar eventos
+export function trackMetaEvent(
+  event: string,
+  params?: Record<string, string | number | boolean>
+) {
+  if (typeof window !== "undefined" && (window as any).fbq) {
+    (window as any).fbq("track", event, params);
+  }
+}
