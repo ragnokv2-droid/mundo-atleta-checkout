9cadf373c2fdf64ec01610e9ac2a6651d24a8423
ragnokv2-droid <ragnokv2@gmail.com>
2026-09-02 01:42:04 -0300

    Update MetaPixel.tsx

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -2,7 +2,10 @@
 
 import Script from "next/script";
 
-const PIXEL_ID = "1387316733571547";
+const PIXEL_IDS = [
+  "1387316733571547",  // Pixel atual
+  "28162540780065698", // Novo Pixel
+];
 
 export default function MetaPixel() {
   return (
@@ -17,18 +20,24 @@ export default function MetaPixel() {
           t.src=v;s=b.getElementsByTagName(e)[0];
           s.parentNode.insertBefore(t,s)}(window, document,'script',
           'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${PIXEL_ID}');
+
+          ${PIXEL_IDS.map((id) => `fbq('init', '${id}');`).join("\n")}
+
           fbq('track', 'PageView');
         `}
       </Script>
+
       <noscript>
-        <img
-          height="1"
-          width="1"
-          style={{ display: "none" }}
-          src={`https://www.facebook.com/tr?id=${PIXEL_ID}&ev=PageView&noscript=1`}
-          alt=""
-        />
+        {PIXEL_IDS.map((id) => (
+          <img
+            key={id}
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${id}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        ))}
       </noscript>
     </>
   );
